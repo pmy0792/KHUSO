21fed5ee80c82af71da6334b4bd1bd40ed3ee14e
pmy0792 <pmy0792@naver.com>
2022-08-09 15:45:39 +0900

    .

--- apimodule.py
+++ apimodule.py
@@ -7,18 +7,20 @@ import requests
 from subprocess import check_output
 
 
-headers = {'Authorization': 'Bearer ' + '191fb802fa3c0d692dfd859587ae8f28f9ee4d22'}
+headers = {'Authorization': 'Bearer ' + '3d59cef697bcc7734a9ec19107ac1618224902f9'}
 
 def execute_api(filename):
     rect=[]
+    result_str=""
     
     command = "node 4.js {}".format(filename.split('.')[0]) 
     result = check_output(command,shell=True).decode(sys.stdout.encoding)
     #p = p[:len(p)-2]
-    print ("result:\n", result)
+    #print ("result:\n", result)
 
     json_obj = json.loads(result)
-    print(len(json_obj["segmentation_results"]))
+    #print(json_obj)
+    #print(len(json_obj["segmentation_results"]))
     
     for obj in json_obj["segmentation_results"]:
         rect.append(obj["contained_bbox"])
@@ -32,11 +34,31 @@ def execute_api(filename):
     
     # get nutrients info
     imgid = json_obj['imageId']
+    print(imgid)
     url = 'https://api.logmeal.es/v2/recipe/nutritionalInfo'
-    resp = requests.post(url,json={'imageId': imgid}, headers=headers)
-    print("food name : ", resp.json())
-    return img, result
+    resp = requests.post(url,json={'imageId': 1448610}, headers=headers)
+    #print("food name : ", resp.json())
+    
+    print(resp)
+    nutri =['CHOCDF','FAT', 'NA' , 'PROCNT']#탄수화물 지방 나트륨 단백질
+
+    #print("calories is:", int(resp.json()['nutritional_info']['calories']))
+    result_str+="Total calories: "+ str(resp.json()['nutritional_info']['calories'])
+    
+    ###########level, percent정보#################
+    for nut in nutri:
+        a =resp.json()['nutritional_info']['dailyIntakeReference']['{}'.format(nut)]
+        #print('{} level, percent is'.format(a['label']), a['level'], int(a['percent']))
+        result_str+="{} level, percent: ".format(a['label'])+ a['level']+ str(a['percent'])
+    ##########자세한 g 정보 #######################
+            
+    for nut in nutri:
+        a =resp.json()['nutritional_info']['totalNutrients']['{}'.format(nut)]
+        #print('{} quantity is'.format(a['label']), int(a['quantity']), a['unit'])
+        result_str+="{} quantity: ".format(a['label'])+ str(a['quantity'])+ a['unit']   
+    
+    return img, result_str
     #cv2.imshow('1', img)
     #cv2.waitKey(0)
     
-execute_api("1.jpg")
+#execute_api("1.jpg")
--- server.py
+++ server.py
@@ -10,26 +10,48 @@ from apimodule import *
 app = Flask(__name__)
 
 
-img_list=[]
+meal_list=[]
 
 @app.route('/', methods=["GET","POST"])
-def home(img_list=[],img=None):
+def home(meal_list=[],img=None):
     if request.method=="GET":
-        return render_template("home.html",img_list=img_list,img=img)
+        return render_template("home.html",meal_list=meal_list,img=img)
     
-    elif request.method=="POST": #image 업로드 들어옴
-        f=request.files['file']
-        f.save(secure_filename(f.filename))
-        img_list.append(f.filename)
-        print("file {} uploaded successfully".format(f.filename))
+    #elif request.method=="POST": 
+    #    return render_template("home.html",meal_list=meal_list,img=f.filename)
+
+@app.route('/upload',methods=["POST"])
+def upload(meal_list=[],img=None):
+    # image upload
+    f=request.files['file']
+    f.save(secure_filename(f.filename))
+    print("file {} uploaded successfully".format(f.filename))
         
-        # run api
-        img, result=execute_api(secure_filename(f.filename))
-        #execute_api(secure_filename('chicken_10'))
-        analyzed_img="static/"+f.filename
-        print("analyzed_img path:" , analyzed_img)
-        cv2.imwrite(analyzed_img,img)
-        return render_template("home.html",img_list=img_list,img=f.filename)
+    # run api
+    img, result=execute_api(secure_filename(f.filename))
+    print("img: ",img)
+    #execute_api(secure_filename('chicken_10'))
+    analyzed_img="static/"+f.filename
+    print("analyzed_img path:" , analyzed_img)
+    cv2.imwrite(analyzed_img,img)
+        
+    # save meal info based on analysis
+    # 여기서 meal name, nutrients info 뽑아내고 사용자가 g수 입력할 수 있도록
+    meal_list.append(f.filename)
+    #meal_list.append({"image":f.filename,"meal_name": meal_name})
+    
+    return render_template("home.html", meal_list=meal_list,img=analyzed_img,result=result)
+
+@app.route('/save',methods=["POST"])
+def save():
+    # meal_list에 모든 정보 저장
+    meal_list=[] 
+    #quantity_data=request.form.items()
+    #for food,quantity in quantity_data:
+    #    # g수에 맞춰 nutrients 계산 후 meal_list update
+     
+    return render_template("home.html",meal_list=meal_list,img=None)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
